e03c3ef749f10e435993308117f7dd775bc62bbf
fatoubin <ndoyemaman200@gmail.com>
2026-02-27 21:22:42 +0000

    fix render port

--- server.js
+++ server.js
@@ -12,19 +12,17 @@ app.use(express.json());
 // ================= CONNEXION MYSQL =================
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("❌ Erreur connexion MySQL:", err);
-  } else {
-    console.log("✅ Connecté à Railway MySQL !");
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
